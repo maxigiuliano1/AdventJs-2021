7b050913314ea1a0071a858b9fd0211b5ce339dc
maxi giuliano <maxigiuliano18@gmail.com>
2021-12-15 12:26:42 -0300

    challenge dia 13 y 14 agregados

---
+++ adventJS-challenge-25dias/challenge-dia13.js
@@ -0,0 +1,43 @@
+/**
+ * ¡Hay demasiados regalos 🎁! Y envolverlos es una locura...
+ * Vamos a crear una función que pasándole un array de regalos, nos devuelva otro array pero donde todos 
+ * los regalos han sido envueltos con asteriscos tanto por arriba como por los lados.
+ * 
+ * Sólo tienes que tener en cuenta unas cosillas ✌️:
+ * 
+ * Si el array está vacío, devuelve un array vacío
+ * Los regalos son emojis 🎁... por lo que tenlo en cuenta a la hora de contar su longitud...
+ * Por suerte, cada posición del array siempre tiene la misma longitud...
+//wrapGifts(["📷", "⚽️"])
+ Resultado:
+[ '****',
+  '*📷*',
+  '*⚽️*',
+  '****'
+]
+
+//wrapGifts(["🏈🎸", "🎮🧸"])
+ Resultado:
+[ '******',
+  '*🏈🎸*',
+  '*🎮🧸*',
+  '******'
+]
+
+//wrapGifts(["📷"])
+ Resultado:
+[ '****',
+  '*📷*',
+  '****'
+]
+*/
+
+function wrapGifts(gifts) {
+
+    let finalGift = []
+    let stringLength = gifts.map(g => g.length)
+    let maxLengthString = Math.max(...stringLength)
+    let boxCover = '*'.repeat(maxLengthString + 2)
+    gifts.forEach(g => finalGift.push(`*${g}*`))
+    return [boxCover,...finalGift,boxCover]
+  }
